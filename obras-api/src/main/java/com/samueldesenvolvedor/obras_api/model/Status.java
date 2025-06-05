2794d2c500d18c23af06ac2ba8e877750d53b000
Samuel Resende <samuelres444@gmail.com>
2025-06-05 13:43:21 -0300

    Extends Jpa para herda todos os metodos CRUD basicos

---
+++ obras-api/src/main/java/com/samueldesenvolvedor/obras_api/model/Status.java
@@ -0,0 +1,15 @@
+package com.samueldesenvolvedor.obras_api.model;
+
+/**
+ * Enum que representa os possíveis estados de uma etapa de obra:
+ * - PENDENTE: etapa ainda não iniciada
+ * - EM_ANDAMENTO: etapa em execução
+ * - CONCLUIDA: etapa finalizada
+ */
+
+
+public enum Status {
+    PENDENTE,
+    EM_ANDAMENTO,
+    CONCLUIDA
+}
---
+++ obras-api/src/main/java/com/samueldesenvolvedor/obras_api/repository/ObraRepository.java
@@ -0,0 +1,18 @@
+package com.samueldesenvolvedor.obras_api.repository;
+
+import com.samueldesenvolvedor.obraapi.model.Obra;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ObraRepository extends JpaRepository<Obra, Long> {
+    // O JpaRepository já fornece:
+    // - save(Obra obra)
+    // - findById(Long id)
+    // - findAll()
+    // - deleteById(Long id)
+    // - e vários outros métodos prontos.
+    
+    // Caso queira adicionar buscas específicas, por exemplo:
+    // List<Obra> findByNomeContaining(String nomeParcial);
+}
