0c4aa876e34efb09d31b5eadce69891bf0e0ea8b
Samuel Resende <samuelres444@gmail.com>
2025-06-05 12:05:16 -0300

    Criacao do projeto no Spring Initializr

---
+++ obras-api/src/main/java/com/samueldesenvolvedor/obras_api/ObrasApiApplication.java
@@ -0,0 +1,13 @@
+package com.samueldesenvolvedor.obras_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObrasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObrasApiApplication.class, args);
+	}
+
+}
---
+++ obras-api/src/test/java/com/samueldesenvolvedor/obras_api/ObrasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samueldesenvolvedor.obras_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObrasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
