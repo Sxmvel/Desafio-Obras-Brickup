ff8ee1c426a62676c2562632d6f139d008e1a075
Samuel Resende <samuelres444@gmail.com>
2025-06-06 11:48:37 -0300

    Implementacao do endpoint DELETE para obra

--- obras-api/src/main/java/com/samueldesenvolvedor/obras_api/controller/ObraController.java
+++ obras-api/src/main/java/com/samueldesenvolvedor/obras_api/controller/ObraController.java
@@ -17,37 +17,47 @@ public class ObraController {
     private ObraService obraService;
 
     @GetMapping
-    public List<Obra> listarTodas() {
+    public List<Obra> listarObras() {
         return obraService.buscarTodasObras();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Obra> buscarPorId(@PathVariable Long id) {
+    public ResponseEntity<Obra> buscarObraPorId(@PathVariable Long id) {
         Optional<Obra> obra = obraService.buscarObraPorId(id);
-        return obra.map(ResponseEntity::ok)
-                   .orElse(ResponseEntity.notFound().build());
+        return obra.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
-    public Obra salvar(@RequestBody Obra obra) {
-        return obraService.salvarObra(obra);
+    public ResponseEntity<Obra> salvarObra(@RequestBody Obra obra) {
+        Obra obraSalva = obraService.salvarObra(obra);
+        return ResponseEntity.ok(obraSalva);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Obra> atualizarObra(@PathVariable Long id, @RequestBody Obra obraAtualizada) {
-        Optional<Obra> optionalObra = obraService.buscarObraPorId(id);
+        Optional<Obra> obraExistente = obraService.buscarObraPorId(id);
+        if (obraExistente.isPresent()) {
+            Obra obra = obraExistente.get();
+            obra.setNome(obraAtualizada.getNome());
+            obra.setDescricao(obraAtualizada.getDescricao());
+            obra.setDataInicio(obraAtualizada.getDataInicio());
+            obra.setDataPrevisaoFim(obraAtualizada.getDataPrevisaoFim());
 
-        if (optionalObra.isEmpty()) {
+            Obra obraSalva = obraService.salvarObra(obra);
+            return ResponseEntity.ok(obraSalva);
+        } else {
             return ResponseEntity.notFound().build();
         }
+    }
 
-        Obra obraExistente = optionalObra.get();
-        obraExistente.setNome(obraAtualizada.getNome());
-        obraExistente.setDescricao(obraAtualizada.getDescricao());
-        obraExistente.setDataInicio(obraAtualizada.getDataInicio());
-        obraExistente.setDataPrevisaoFim(obraAtualizada.getDataPrevisaoFim());
-
-        Obra obraSalva = obraService.salvarObra(obraExistente);
-        return ResponseEntity.ok(obraSalva);
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarObra(@PathVariable Long id) {
+        Optional<Obra> obra = obraService.buscarObraPorId(id);
+        if (obra.isPresent()) {
+            obraService.deletarObra(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
